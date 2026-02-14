56d5d32b863c3c502361cdae696e3c70f9169099
Yug Shah <shahyug91@gmail.com>
2026-02-14 16:37:16 +0530

    Updates

--- Spring/src/main/java/com/example/Spring/Application.java
+++ Spring/src/main/java/com/example/Spring/Application.java
@@ -1,7 +1,6 @@
 package com.example.Spring;
 
-import
-		org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
